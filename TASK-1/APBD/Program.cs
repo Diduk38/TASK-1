7f2a5df71f46e710bf257ed585fdbd48b2b7bdc7
s27736 <s27736@pjwstk.edu.pl>
2024-03-15 11:56:44 +0300

    Added Class Program and method average

--- TASK-1/APBD/Program.cs
+++ TASK-1/APBD/Program.cs
@@ -1,5 +1,33 @@
-﻿Console.WriteLine("Modification 1");
+﻿
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Modification 1");
 
-Console.WriteLine("Modification 2.");
+        Console.WriteLine("Modification 2.");
 
-Console.WriteLine("Modification 3.");
+        Console.WriteLine("Modification 3.");
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        double averageOfArray=Average(arr);
+        
+        Console.WriteLine(averageOfArray);
+    }
+
+    static double Average(int[] array)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+
+        }
+
+        double averageVariable = sum / array.Length;
+        return averageVariable;
+    }
+
+}
+
